11f84a6f04035ee9c03b6c0cf061a8a953a3e85e
ewd1234 <jacoblorenzen02@gmail.com>
2025-11-12 13:31:40 +0100

    Slettet fra app

--- public/app.js
+++ public/app.js
@@ -18,26 +18,3 @@ export async function loadSongs() {
   }
 }
 
-function displaySongs(song) {
-  const songList = document.getElementById('songList');
-  songList.innerHTML = '';
-  
-  song.forEach(sang => {
-    const songRow = document.createElement('div');
-    songRow.className = 'song-row';
-    songRow.innerHTML = `
-      <img src="${sang.cover_image}" alt="${sang.name}" class="song-cover">
-      <div class="song-info">
-        <div class="song-name">${sang.name}</div>
-        <div class="song-artist">${sang.artist_name}</div>
-        <div class"song-duration">${sang.duration}</div>
-      </div>
-      <br>
-    `;
-    songList.appendChild(songRow);
-  });
-}
-
-
-// Start når siden loader
-loadSongs();
